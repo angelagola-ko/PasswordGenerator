71ff8c1f83d161cd6aeae831605f79c2cd3265ab
Angela Gola <95582357+angelaa1794@users.noreply.github.com>
2022-04-21 20:58:45 -0400

    Deleted irrelevant code

--- Develop/script.js
+++ Develop/script.js
@@ -62,10 +62,6 @@ var STARTING_RANGE_UPPERCASE_CODE = 65;
 var ENDING_RANGE_UPPERCASE_CODE = 90;
 var STARTING_RANGE_LOWERCASE_CODE = 97;
 var ENDING_RANGE_LOWERCASE_CODE = 122;
-//var THE_SPECIAL_CHARACTER_CODE = arrayFromLowToHigh(33,47)
-//.concat(arrayFromLowToHigh(58,64))
-//.concat(arrayFromLowToHigh(91,96))
-//.concat(arrayFromLowToHigh(123,126));//notworking.
 var THE_SPECIAL_CHARACTER_CODE = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')',
   '(','}','{',']','[','~','-','_','.'];
 var STARTING_NUMBER_CODE = 48;
@@ -143,7 +139,7 @@ function generatedPassword() {
 
   if (wantsLowerCaseLetters) {
     var randomLowerCaseLetter = lowerCaseLetterPool[
-      Math.floor(Math.random() * lowerCaseLetterPool.length + 1)];
+      Math.floor(Math.random() * lowerCaseLetterPool.length)];
     console.log("random lowercase", randomLowerCaseLetter);
     generatedPassword += randomLowerCaseLetter;
     characterPool = characterPool.concat(lowerCaseLetterPool);
@@ -151,26 +147,21 @@ function generatedPassword() {
   
   if (wantsUpperCaseLetters) {
     var randomUpperCaseLetter = upperCaseLetterPool[
-      Math.floor(Math.random() * upperCaseLetterPool.length + 1)];
+      Math.floor(Math.random() * upperCaseLetterPool.length)];
     console.log("random uppercase", randomUpperCaseLetter);
     generatedPassword += randomUpperCaseLetter;
     characterPool = characterPool.concat(upperCaseLetterPool);
   }
 
   if (wantsNumbers) {
-    var randomNumber = numberPool[Math.floor(Math.random() * numberPool.length + 1)];
+    var randomNumber = numberPool[Math.floor(Math.random() * numberPool.length)];
     console.log("random number", randomNumber);
     generatedPassword += randomNumber;
     characterPool = characterPool.concat(numberPool);
-//    if (randomNumber === 0) {
-//      generatedPassword += 0;
-//      console.log("random number", 0);
-//    }
-
   }
 
   if(wantsSpecialCharacters) {
-    var randomSpecialCharacter = THE_SPECIAL_CHARACTER_CODE[Math.floor(Math.random() * THE_SPECIAL_CHARACTER_CODE.length +1)];
+    var randomSpecialCharacter = THE_SPECIAL_CHARACTER_CODE[Math.floor(Math.random() * THE_SPECIAL_CHARACTER_CODE.length)];
     console.log("random special character", randomSpecialCharacter);
     generatedPassword += randomSpecialCharacter;
     characterPool = characterPool.concat(THE_SPECIAL_CHARACTER_CODE);
@@ -186,136 +177,9 @@ function generatedPassword() {
   return generatedPassword;
 }
 generatedPassword();
-//random special character undefined
-//random number undefined - FOR ZERO '0'
-
-//function arrayFromLowToHigh(low, high) {
-//  const array = [];
-//  for (let i = low; i <= high; i++) {
-//    array.push(i)
-//  }
-//  return array
-//}//not working
 
 
-
-/*var numbers= '1234567890';
-var upperCase='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lowerCase='abcdefghijklmnopqrstuv';
-var symbols='!@#$%^&*(){}[]';
-
-function passwordprompt() {
-  var length = window.prompt('How  long would you like your password to be, between 8-128 characters?');
-  var isCharUpper = null;
-  var isCharLower = null;
-  var isCharSymbol = null;
-  var isCharNumber = null;
-
-  if (confirm("Would you like uppercase letters?") == true) {
-    isCharUpper = true;
-} else {
-    isCharUpper = false;
-}
-if (confirm("Would you like lowercase letters?") == true) {
-  isCharLower = true;
-} else {
-  isCharLower = false;
-}
-if (confirm("Would you like symbols letters?") == true) {
-  isCharSymbol = true;
-} else {
-  isCharSymbol = false;
-}
-if (confirm("Would you like numbers letters?") == true) {
-  isCharNumber = true;
-} else {
-  isCharNumber = false;
-}
-
-if (!isCharUpper && !isCharLower && !isCharNumber && !isCharSymbol) {
-  window.alert('You have not selected anything. Please pick at least one');
-}
-
-var password = "";
-var segmentOfPw = length/4;
-var lastSegmentOfPw = length - (segmentOfPw * 3);
-
-};
-
-
-//Do you want a new pw?
-//How long do you want your password to be, between 8-128 char?
-//Do you want uppercase?
-//Do you want lowercase?
-//Do you want numbers?
-//Do you want special characters?
-//you must enter a vailid pw number
-// You must pick at least one option*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const LOWERCASE_CHAR_CODES = arrayLowtoHigh(65,90);
-const UPPERCASE_CHAR_CODES = arrayLowtoHigh(97,122);
-const NUMBER_CHAR_CODES = arrayLowtoHigh(48,57);
-const SYMBOL_CHAR_CODES = arrayLowtoHigh(33,47).concat(
-  arrayLowtoHigh(58,64)
-).concat(
-  arrayLowtoHigh(91,96)
-).concat(
-  arrayLowtoHigh(123,126)
-);
-
-
-function genPW(charAmt, includeUpper, includeLower, includeSymbols, includeNumbers) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUpper) charCodes = charCodes.concat
-  (UPPERCASE_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat
-  (SYMBOL_CHAR_CODES)
-  if(includeNumbers) charCodes = charCodes.concat
-  (NUMBER_CHAR_CODES)
-
-  const password = []
-  for (let i = 0; i < charAmt, i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * charAmt)]
-    password.push(Spring.fromCharCode(characterCode)
-  }
-  console.log(LOWERCASE_CHAR_CODES);/*not printing*/
-/*}
-
-
-function arrayLowtoHigh(low, high) {
-  const array = []
-  for (let i = low; i <=high; i++);
-  array.push(i);
-  return array;
-}
-
-// Assignment code here
-
+/*
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -328,6 +192,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);*/
+//generateBtn.addEventListener("click", writePassword);*/
